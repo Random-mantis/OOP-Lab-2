75599a6b60b8143164e2c698fe8e889bf59b66b6
Random-mantis <kstgnbk@gmail.com>
2025-10-16 18:38:50 +0300

    Add files via upload

--- programm.cpp
+++ programm.cpp
@@ -6,11 +6,13 @@ int main() {
     Decimal b(12345);
     Decimal c("6789");
     Decimal d({9,8,7,6});
+    Decimal d2(d);
 
     std::cout << "a = "; a.printTo(); std::cout << "\n";
     std::cout << "b = "; b.printTo(); std::cout << "\n";
     std::cout << "c = "; c.printTo(); std::cout << "\n";
     std::cout << "d = "; d.printTo(); std::cout << "\n";
+    std::cout << "&d = " << &d << "&d2 = " << &d2 << "\n";
 
     Decimal sum = b.sum(c);
     Decimal diff = b.addition_minus(c);
